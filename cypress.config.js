6110a97f5baf3eac222e9980516aa548a2e2dce8
Johan Ballen <alexanderfm1989@gmail.com>
2026-02-20 11:22:58 -0500

    Add Cypress Cloud projectId for CI recording

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'tyngys',
   e2e: {
     baseUrl: 'https://buggy.justtestit.org/',
     screenshotOnRunFailure: true,
+    video: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
