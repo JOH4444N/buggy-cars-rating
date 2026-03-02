f1bfd0b9c6895a5d369688c9a09dcffd7bee638e
Johan Ballen <alexanderfm1989@gmail.com>
2026-03-02 18:34:12 -0500

    generar reportes CD

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,15 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: 'tyngys',
+  
+  reporter: "mochawesome",
+  reporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: false,
+      html: false,
+      json: true
+  },
+  
   e2e: {
     baseUrl: 'https://buggy.justtestit.org/',
     screenshotOnRunFailure: true,
