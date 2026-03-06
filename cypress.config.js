b486711f8d31586e7866781da2678827a4ef3512
Johan Ballen <alexanderfm1989@gmail.com>
2026-03-05 20:52:15 -0500

    Generar reportes con mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,14 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: 'tyngys',
+
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: false,
+    json: true
+  },
   
   e2e: {
     baseUrl: 'https://buggy.justtestit.org/',
